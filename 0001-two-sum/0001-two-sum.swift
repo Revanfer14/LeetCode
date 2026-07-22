6c1c97acb4ade16c46de25a548a43bf9f85a75be
Revan Ferdinand <140201920+Revanfer14@users.noreply.github.com>
2026-07-22 09:35:01 +0700

    Time: 0 ms (100%), Space: 20 MB (42.57%) - LeetHub

--- 0001-two-sum/0001-two-sum.swift
+++ 0001-two-sum/0001-two-sum.swift
@@ -4,17 +4,12 @@ class Solution {
 
         for (i, n) in nums.enumerated() {
             let complement = target - n
-            print(complement)
-
-            // 2:0 , 7:1
             
             if let j = seen[complement] {
                 return [j, i]
             }
 
             seen[n] = i
-
-            
         }
 
         return []
